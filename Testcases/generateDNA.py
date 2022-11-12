58cced407079fbeb520992738c32d58e0378726f
Naman Kashyap <107040799+namskash@users.noreply.github.com>
2022-11-12 16:58:39 +0530

    Scaled from sample

--- Testcases/generateDNA.py
+++ Testcases/generateDNA.py
@@ -1,4 +1,4 @@
-from random import choice
+from random import choice,randint
 
 def printListAsString(text):				# because strings are immutable, concatenating 100s of times didn't seem like a good choice
 	for word in text:
@@ -8,13 +8,13 @@ def printListAsString(text):				# because strings are immutable, concatenating 1
 #% 4. DNA sequence:
 DNA = ['A','T','G','C']
 
-for length in range(25,50,5):
+for length in range(250,500,5):			# (25,50,5) for sample
 	text = []
 	for i in range(length):
 		text.append(choice(DNA))
-	for i in range(0,20,5):
+	for i in range(0,length,5):
 		printListAsString(text)				# print text
-		pattern = text[i:i + 5]				# slice text to get pattern
+		pattern = text[i:i + randint(5,10)]				# slice text to get pattern
 		printListAsString(pattern)
 	
 	printListAsString(text)					# print text one last time
