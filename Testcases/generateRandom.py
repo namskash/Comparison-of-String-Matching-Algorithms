f06ba4f2dc0b3006a2bde59e716e725e3ecdd1ab
Naman Kashyap <107040799+namskash@users.noreply.github.com>
2022-11-12 16:58:06 +0530

    Scaled from sample

--- Testcases/generateRandom.py
+++ Testcases/generateRandom.py
@@ -1,5 +1,5 @@
-from random import choice
-from string import ascii_letters,digits,punctuation,printable
+from random import choice,randint
+from string import ascii_letters,digits,punctuation
 
 def printListAsString(text):				# because strings are immutable, concatenating 100s of times didn't seem like a good choice
 	for word in text:
@@ -13,13 +13,13 @@ for i in range(100):						# generate random characters from ASCII
 	characters.append(choice(punctuation))
 	characters.append(choice(digits))
 
-for length in range(25,50,5):
+for length in range(250,500,5):			# (25,50,5) for sample
 	text = []
 	for i in range(length):					# generate random characters from ASCII
 		text.append(choice(characters))
-	for i in range(0,20,5):
+	for i in range(0,length,5):
 		printListAsString(text)				# print text
-		pattern = text[i:i + 5]				# slice text to get pattern
+		pattern = text[i:i + randint(5,10)]				# slice text to get pattern
 		printListAsString(pattern)
 	
 	printListAsString(text)					# print text one last time
